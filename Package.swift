34f03abbfb38656f2d67639aaa28eeef6fb2dd8d
Alex Vera <69604865+c128128@users.noreply.github.com>
2022-01-18 17:29:21 +0100

    fixed: Google Ads Deps

--- Package.swift
+++ Package.swift
@@ -10,26 +10,33 @@ let package = Package(
         .library(name: "AdsKit", targets: ["AdsKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(name: "nanopb", url: "https://github.com/firebase/nanopb.git", .exact("2.30908.0")),
+        .package(name: "GoogleUtilities", url: "https://github.com/google/GoogleUtilities.git", .exact("7.6.0")),
+        .package(name: "Promises", url: "https://github.com/google/promises.git", .exact("2.0.0")),
+        .package(name: "GoogleAppMeasurement", url: "https://github.com/google/GoogleAppMeasurement.git", .exact("8.9.1"))
     ],
     targets: [
-        .binaryTarget(name: "GoogleAppMeasurement", path: "Sources/GoogleMobileAds/GoogleAppMeasurement.xcframework"),
-        .binaryTarget(name: "GoogleAppMeasurementIdentitySupport", path: "Sources/GoogleMobileAds/GoogleAppMeasurementIdentitySupport.xcframework"),
         .binaryTarget(name: "GoogleMobileAds", path: "Sources/GoogleMobileAds/GoogleMobileAds.xcframework"),
-        .binaryTarget(name: "GoogleUtilities", path: "Sources/GoogleMobileAds/GoogleUtilities.xcframework"),
-        .binaryTarget(name: "nanopb", path: "Sources/GoogleMobileAds/nanopb.xcframework"),
-        .binaryTarget(name: "PromisesObjC", path: "Sources/GoogleMobileAds/PromisesObjC.xcframework"),
         .binaryTarget(name: "UserMessagingPlatform", path: "Sources/GoogleMobileAds/UserMessagingPlatform.xcframework"),
         .target(name: "AdsKitAutoload"),
         .target(
             name: "AdsKit",
             dependencies: [
-                .byName(name: "GoogleAppMeasurement", condition: .when(platforms: [.iOS])),
-                .byName(name: "GoogleAppMeasurementIdentitySupport", condition: .when(platforms: [.iOS])),
+                .product(name: "GoogleAppMeasurement", package: "GoogleAppMeasurement", condition: .when(platforms: [.iOS, .macOS, .tvOS])),
+                
+                .product(name: "GULAppDelegateSwizzler", package: "GoogleUtilities"),
+                .product(name: "GULEnvironment", package: "GoogleUtilities"),
+                .product(name: "GULLogger", package: "GoogleUtilities"),
+                .product(name: "GULISASwizzler", package: "GoogleUtilities"),
+                .product(name: "GULMethodSwizzler", package: "GoogleUtilities"),
+                .product(name: "GULNetwork", package: "GoogleUtilities"),
+                .product(name: "GULNSData", package: "GoogleUtilities"),
+                .product(name: "GULReachability", package: "GoogleUtilities"),
+                .product(name: "GULUserDefaults", package: "GoogleUtilities"),
+                "nanopb",
+                .product(name: "FBLPromises", package: "Promises"),
                 .byName(name: "GoogleMobileAds", condition: .when(platforms: [.iOS])),
-                .byName(name: "GoogleUtilities", condition: .when(platforms: [.iOS])),
-                .byName(name: "nanopb", condition: .when(platforms: [.iOS])),
-                .byName(name: "PromisesObjC", condition: .when(platforms: [.iOS])),
                 .byName(name: "UserMessagingPlatform", condition: .when(platforms: [.iOS])),
                 "RxSwift",
                 .product(name: "RxCocoa", package: "RxSwift"),
